ef80e246d7d17e74fffe592bcbed123556dfb182
john <jnkroeker@gmail.com>
2021-11-06 11:07:00 -0400

    set up debugging support for use during development

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"errors"
-<<<<<<< HEAD
 	"expvar"
-=======
->>>>>>> override default configuration with environment variables or command line flags
 	"fmt"
 	"net/http"
 	"os"
@@ -14,11 +11,8 @@ import (
 	"syscall"
 	"time"
 
-<<<<<<< HEAD
 	"jnk-ardan-service/app/services/sales-api/handlers"
 
-=======
->>>>>>> override default configuration with environment variables or command line flags
 	"github.com/ardanlabs/conf"
 	"go.uber.org/automaxprocs/maxprocs"
 	"go.uber.org/zap"
