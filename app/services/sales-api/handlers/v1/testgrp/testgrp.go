eb307ad0c0695ba430a006aa9e8c1380899e124d
john <jnkroeker@gmail.com>
2021-11-09 19:40:08 -0500

    add business layer logging so logger can be taken out of app layer handler function

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -1,15 +1,11 @@
 package testgrp
 
 import (
-<<<<<<< HEAD
 	"context"
 	"errors"
 	"jnk-ardan-service/business/sys/validate"
 	"jnk-ardan-service/foundation/web"
 	"math/rand"
-=======
-	"encoding/json"
->>>>>>> add handlers
 	"net/http"
 
 	"go.uber.org/zap"
@@ -21,7 +17,6 @@ type Handlers struct {
 }
 
 // Test handler is for development
-<<<<<<< HEAD
 func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 
 	if n := rand.Intn(100); n%2 == 0 {
@@ -30,22 +25,11 @@ func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Reque
 		// panic("testing panic")
 	}
 
-=======
-func (h Handlers) Test(w http.ResponseWriter, r *http.Request) {
->>>>>>> add handlers
 	status := struct {
 		Status string
 	}{
 		Status: "OK",
 	}
-<<<<<<< HEAD
 
 	return web.Respond(ctx, w, status, http.StatusOK)
-=======
-	json.NewEncoder(w).Encode(status)
-
-	statusCode := http.StatusOK
-
-	h.Log.Infow("readiness", "statusCode", statusCode, "method", r.Method, "path", r.URL.Path, "remoteaddr", r.RemoteAddr)
->>>>>>> add handlers
 }
