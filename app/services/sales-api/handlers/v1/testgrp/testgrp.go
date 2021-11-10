3b2213456e13ef0222ab550f7312b12735743ea8
john <jnkroeker@gmail.com>
2021-11-09 21:12:05 -0500

    add error middleware leveraging error types

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,7 +2,10 @@ package testgrp
 
 import (
 	"context"
+	"errors"
+	"jnk-ardan-service/business/sys/validate"
 	"jnk-ardan-service/foundation/web"
+	"math/rand"
 	"net/http"
 
 	"go.uber.org/zap"
@@ -15,6 +18,12 @@ type Handlers struct {
 
 // Test handler is for development
 func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+
+	if n := rand.Intn(100); n%2 == 0 {
+		// return errors.New("untrusted error")
+		return validate.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+	}
+
 	status := struct {
 		Status string
 	}{
