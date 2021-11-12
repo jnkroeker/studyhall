0f033710ab643c98200721a877a50f8f533667d9
john <jnkroeker@gmail.com>
2021-11-11 20:14:13 -0500

    access database from within app using convenience methods around sqlx

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -14,10 +14,7 @@ import (
 
 	"jnk-ardan-service/app/services/sales-api/handlers"
 	"jnk-ardan-service/business/sys/auth"
-<<<<<<< HEAD
 	"jnk-ardan-service/business/sys/database"
-=======
->>>>>>> integration of auth middleware inside the application
 	"jnk-ardan-service/foundation/keystore"
 
 	"github.com/ardanlabs/conf"
@@ -78,7 +75,6 @@ func run(log *zap.SugaredLogger) error {
 			KeysFolder string `conf:"default:zarf/keys/"`
 			ActiveKID  string `conf:"default:54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"`
 		}
-<<<<<<< HEAD
 		DB struct {
 			User         string `conf:"default:postgres"`
 			Password     string `conf:"default:postgres,mask"`
@@ -88,8 +84,6 @@ func run(log *zap.SugaredLogger) error {
 			MaxOpenConns int    `conf:"default:0"`
 			DisableTLS   bool   `conf:"default:true"`
 		}
-=======
->>>>>>> integration of auth middleware inside the application
 	}{
 		Version: conf.Version{
 			SVN:  build,
@@ -139,7 +133,6 @@ func run(log *zap.SugaredLogger) error {
 	}
 
 	// ========================================================================================
-<<<<<<< HEAD
 	// Database Support
 
 	// Create connectivity to the database.
@@ -163,8 +156,6 @@ func run(log *zap.SugaredLogger) error {
 	}()
 
 	// ========================================================================================
-=======
->>>>>>> integration of auth middleware inside the application
 	// Start Debug Service
 
 	log.Infow("startup", "status", "debug router started", "host", cfg.Web.DebugHost)
@@ -198,10 +189,7 @@ func run(log *zap.SugaredLogger) error {
 		Shutdown: shutdown,
 		Log:      log,
 		Auth:     auth,
-<<<<<<< HEAD
 		DB:       db,
-=======
->>>>>>> integration of auth middleware inside the application
 	})
 
 	// Construct a server to service the requests against the mux.
