fa2987ccb18b1b4617b34023cc2d4b9bf3a1ddb3
john <jnkroeker@gmail.com>
2021-11-11 20:14:13 -0500

    access database from within app using convenience methods around sqlx

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -14,6 +14,7 @@ import (
 
 	"jnk-ardan-service/app/services/sales-api/handlers"
 	"jnk-ardan-service/business/sys/auth"
+	"jnk-ardan-service/business/sys/database"
 	"jnk-ardan-service/foundation/keystore"
 
 	"github.com/ardanlabs/conf"
@@ -74,6 +75,15 @@ func run(log *zap.SugaredLogger) error {
 			KeysFolder string `conf:"default:zarf/keys/"`
 			ActiveKID  string `conf:"default:54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"`
 		}
+		DB struct {
+			User         string `conf:"default:postgres"`
+			Password     string `conf:"default:postgres,mask"`
+			Host         string `conf:"default:localhost"`
+			Name         string `conf:"default:postgres"`
+			MaxIdleConns int    `conf:"default:0"`
+			MaxOpenConns int    `conf:"default:0"`
+			DisableTLS   bool   `conf:"default:true"`
+		}
 	}{
 		Version: conf.Version{
 			SVN:  build,
@@ -121,6 +131,29 @@ func run(log *zap.SugaredLogger) error {
 	if err != nil {
 		return fmt.Errorf("constructing auth: %w", err)
 	}
+
+	// ========================================================================================
+	// Database Support
+
+	// Create connectivity to the database.
+	log.Infow("startup", "status", "initializing database support", "host", cfg.DB.Host)
+
+	db, err := database.Open(database.Config{
+		User:         cfg.DB.User,
+		Password:     cfg.DB.Password,
+		Host:         cfg.DB.Host,
+		Name:         cfg.DB.Name,
+		MaxIdleConns: cfg.DB.MaxIdleConns,
+		MaxOpenConns: cfg.DB.MaxOpenConns,
+		DisableTLS:   cfg.DB.DisableTLS,
+	})
+	if err != nil {
+		return fmt.Errorf("connecting to db: %w", err)
+	}
+	defer func() {
+		log.Infow("shutdown", "status", "stopping database support", "host", cfg.DB.Host)
+		db.Close()
+	}()
 
 	// ========================================================================================
 	// Start Debug Service
--- makefile
+++ makefile
@@ -16,6 +16,9 @@ SHELL := /bin/bash
 # curl -il http://localhost:3000/v1/testauth
 # curl -H "Authorization: Bearer ${TOKEN}" http://localhost:3000/v1/testauth
 
+# Database Access (github.com/danvergara/dblab)
+# dblab --host 0.0.0.0 --user postgres --db postgres --pass postgres --ssl disable --port 5432 --driver postgres
+
 # ============================================================================
 
 run:
